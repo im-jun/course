b900306bfcaecae9eca6b6ca4378c584ab703f83
im-jun <2277651605@qq.com>
2023-06-01 00:27:18 +0800

    '二分法整理'

--- Algorithm_learn/二分法.cpp
+++ Algorithm_learn/二分法.cpp
@@ -4,15 +4,10 @@ using namespace std;
   使用前提：数据有序
   时间复杂度：O(log2^N) 简写成--> O(logN)
  */
-int main()
+int bm(int a[], int n, int num) // n为数组长度 num为要找的指定数
 {
-	int num;
-	cin >> num;
-	// 返回num出现的位置
-	int a[10] = {1, 2, 2, 3, 3, 5, 6, 7, 45, 67};
-	
 	int l = 0;
-	int r = 9;
+	int r = n - 1;
 	int mid = 0;
 	while(l < r)
 	{
@@ -23,8 +18,7 @@ int main()
 		mid = l + ((r - l) >> 1); 
 		if(a[mid] == num)
 		{
-			cout << mid;
-			return 0;
+			return mid;
 		}
 		else if(a[mid] > num)
 		{
@@ -36,6 +30,15 @@ int main()
 		}
 	}
 	
-	cout << a[l];
+	return l;
+}
+
+int main()
+{
+	int num;
+	cin >> num;
+	// 返回num出现的位置
+	int a[10] = {1, 2, 2, 3, 3, 5, 6, 7, 45, 67};
+	cout << bm(a, 10, num);
 	return 0;
 }
